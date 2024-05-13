366e92ce655b308d8421010a39cb61bea439fa8a
Ag <theankushgautam@gmail.com>
2024-05-13 22:38:22 +0545

    Complete test #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (firstNum, lastNum) {
+  if (
+    firstNum < 0 ||
+    lastNum < 0 ||
+    typeof firstNum != "number" ||
+    typeof lastNum != "number"
+  )
+    return "ERROR";
 
+  let sum = 0;
+
+  // if last number is smaller
+  if (lastNum < firstNum) {
+    let temp = lastNum;
+    lastNum = firstNum;
+    firstNum = temp;
+  }
+
+  for (let i = firstNum; i <= lastNum; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
