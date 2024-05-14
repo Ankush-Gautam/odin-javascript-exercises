4613459a8fd2f139c9d1fd8d3017b96cf7c949c9
Ag <theankushgautam@gmail.com>
2024-05-14 20:56:37 +0545

    Complete test #9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let array = string.toLowerCase().split(""); //converting to array with lowercase letters
 
+  let punctuations = [
+    ",",
+    ".",
+    "/",
+    "~",
+    "!",
+    "-",
+    "_",
+    "#",
+    "%",
+    "^",
+    "&",
+    "*",
+    " ",
+  ];
+
+  let filteredArray = array.filter((item) => !punctuations.includes(item));
+
+  let originalFilteredString = filteredArray.join("");
+  let reversedString = filteredArray.reverse().join("");
+
+  console.log(originalFilteredString);
+  console.log(reversedString);
+
+  return originalFilteredString === reversedString;
 };
+
+palindromes("Animal loots foliated detail of stool lamina.");
 
 // Do not edit below this line
 module.exports = palindromes;
