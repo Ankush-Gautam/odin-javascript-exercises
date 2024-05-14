ad42d71d52141e3236d8153a2e4ccd4ad6d1524c
Ag <theankushgautam@gmail.com>
2024-05-14 22:13:42 +0545

    Complete TTD exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let index = parseInt(n);
+  if (index == 0) return 0;
+  if (index < 0) return "OOPS";
 
+  let firstNum = 1;
+  let secondNum = 1;
+  let fibArray = [firstNum, secondNum];
+  let sum = 0;
+
+  for (let i = 2; i < index; i++) {
+    sum = fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2];
+    fibArray.push(sum);
+  }
+
+  return fibArray[index - 1];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPersonAge = calcAge(people[0].yearOfBirth, people[0].yearOfDeath);
+  let oldestPerson = people[0];
 
+  for (let i = 0; i < people.length; i++) {
+    if (
+      calcAge(people[i].yearOfBirth, people[i].yearOfDeath) > oldestPersonAge
+    ) {
+      oldestPersonAge = calcAge(people[i].yearOfBirth, people[i].yearOfDeath);
+      oldestPerson = people[i];
+    }
+  }
+  return oldestPerson;
 };
+
+function calcAge(birthYear, deathYear) {
+  if (deathYear != undefined) return deathYear - birthYear;
+
+  const currentYear = new Date();
+  return currentYear.getFullYear() - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
