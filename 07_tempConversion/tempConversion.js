dd50d75733e1cb028a2a0892825ad3a3d78163d1
Ag <theankushgautam@gmail.com>
2024-05-14 19:51:23 +0545

    Complete test #7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  // formula to C = (5/9) * F - 32
+  return parseFloat(((5 / 9) * (temperature - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  // f = (9/5)C + 32;
+  return parseFloat(((9 / 5) * temperature + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
